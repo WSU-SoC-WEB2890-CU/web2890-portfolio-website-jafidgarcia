2a286993ecaeed2f385ab89da6405b0789b81b46
Jafid Garcia <jafidgarcia@gmail.com>
2023-03-21 22:18:29 -0600

    added javascript for slide in effects

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,2 +1,43 @@
 //TODO - Your ES6 JavaScript code (if any) goes here
 import "bootstrap"
+
+
+
+const hrElements = document.querySelectorAll('.moves');
+
+hrElements.forEach((hrElements,index)=>{
+hrElements.classList.add('slide-in');
+
+hrElements.id = `section-${index + 1}`;
+
+  // Create an IntersectionObserver for each section
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.intersectionRatio > 0) {
+        entry.target.classList.add('show');
+      }
+    });
+  });
+
+  // Observe the section
+  observer.observe(hrElements);
+});
+
+
+
+
+//  cant get to work correct
+// const hrElements = document.querySelectorAll('.moves');
+
+// function animateHrElements() {
+//   hrElements.forEach(hrElement => {
+//     const hrOffset = hrElement.getBoundingClientRect().top;
+//     const hasAnimated = hrElement.classList.contains('slide-in');
+
+//     if (!hasAnimated && window.scrollY >= hrOffset) {
+//       hrElement.classList.add('slide-in');
+//     }
+//   });
+// }
+
+// window.addEventListener('scroll', animateHrElements);
