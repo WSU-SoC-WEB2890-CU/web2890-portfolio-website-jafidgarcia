6ec1bddda83849028359260e71f41a625d2dea77
Jafid Garcia <jafidgarcia@gmail.com>
2023-04-19 23:58:43 -0600

    fixed pdf deploy > assets  issue with netlify

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,21 @@
 
-const path = require("path")
-const glob = require("glob")
+const path = require("path");
+const glob = require("glob");
 
-import { defineConfig } from "vite"
-import eslint from "@rollup/plugin-eslint"
-// import copy from "vite-plugin-copy";
+import { defineConfig } from "vite";
+import eslint from "@rollup/plugin-eslint";
+import copy from 'rollup-plugin-copy';
 
-let input = {}
+let input = {};
 
 glob
   .sync("./src/**/*.html")
   .map((file) => {
-    return [file.substring(`.${path.sep}src${path.sep}`.length), file]
+    return [file.substring(`.${path.sep}src${path.sep}`.length), file];
   })
   .forEach((f) => {
-    input[f[0]] = f[1]
-  })
+    input[f[0]] = f[1];
+  });
 
 export default defineConfig({
   root: "src",
@@ -24,7 +24,7 @@ export default defineConfig({
     outDir: "../dist",
     emptyOutDir: true,
     rollupOptions: {
-      input
+      input,
     },
   },
   server: {
@@ -39,5 +39,61 @@ export default defineConfig({
       enforce: "pre",
       apply: "build",
     },
+    copy({
+      targets: [
+        {
+          src: 'src/**/*.pdf',
+          dest: '../dist',
+        },
+      ],
+      flatten: false,
+    }),
   ],
-})
+});
+
+
+
+
+// original 
+// const path = require("path")
+// const glob = require("glob")
+
+// import { defineConfig } from "vite"
+// import eslint from "@rollup/plugin-eslint"
+// import copy from "vite-plugin-copy";
+
+// let input = {}
+
+// glob
+//   .sync("./src/**/*.html")
+//   .map((file) => {
+//     return [file.substring(`.${path.sep}src${path.sep}`.length), file]
+//   })
+//   .forEach((f) => {
+//     input[f[0]] = f[1]
+//   })
+
+// export default defineConfig({
+//   root: "src",
+//   build: {
+//     manifest: true,
+//     outDir: "../dist",
+//     emptyOutDir: true,
+//     rollupOptions: {
+//       input
+//     },
+//   },
+//   server: {
+//     port: 8080,
+//     open: "/",
+//   },
+//   plugins: [
+//     {
+//       ...eslint({
+//         include: ["src/**/*.js"],
+//       }),
+//       enforce: "pre",
+//       apply: "build",
+//     },
+//   ],
+// })
